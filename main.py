909126cf4cf1ff5519b5ed08e690ae6c523ed210
BatteRaquette581 <84451047+BatteRaquette581@users.noreply.github.com>
2022-02-08 11:42:41 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+from sys import *
+a=[]
+for i in range(9999):a.append(0)
+f=open("bfCopy.txt","r").read()
+def p(m):
+ global c
+ c=0
+ w=""
+ l=0
+ for i in m:
+  if i=="(":
+   l=1
+   continue
+  if i==")":
+   l=0
+   while a[c]!=0:p(w)
+  if l:
+   w+=i
+   continue
+  if i=="^":c+=1
+  if i=="v":c-=1
+  if i=="i":a[c]+=1
+  if i=="d":a[c]-=1
+  if i=="!":stdout.write(chr(a[c]))
+  if i=="?":a[c]=int(input())
+  if i=="§":stdout.write(str(a[c]))
+  if i=="m":stdout.write(str(a))
+p(f)
